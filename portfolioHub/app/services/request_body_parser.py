7c1950e842b302d96ae464fb40b23873b43ce837
JGVR <juangabrielvasquez11@gmail.com>
2024-12-08 08:26:20 -0500

    updated the request body parser to us the new user, and profile class

--- portfolioHub/app/services/request_body_parser.py
+++ portfolioHub/app/services/request_body_parser.py
@@ -1,17 +1,18 @@
 from .model_identifier import ModelIdentifier
-from ..models.profile import Person
+from ..models.profile import Profile
 from ..models.achievement import Achievement
 from ..models.experience import Experience
 from ..models.project import Project
+from ..models.user import User
 from typing import Dict, Any, Union, List
 
 class RequestBodyParser:
     @staticmethod
-    def parse_request_body(request_body: Dict[str,Any]) -> Union[Person, Achievement, Project, Experience] | List[Union[Person, Achievement, Project, Experience]]:
-        collection_name = request_body["collection"]
+    def parse_request_body(request_body: Dict[str,Any]) -> Union[User, Profile, Achievement, Project, Experience] | List[Union[User, Profile, Achievement, Project, Experience]]:
+        model_type = request_body["type"]
 
         #check if the request body has more than 1 record
         if isinstance(request_body["data"], list):
-            results = [ModelIdentifier.identify_model(collection_name, model_data) for model_data in request_body["data"]]
+            results = [ModelIdentifier.identify_model(model_type, model_data) for model_data in request_body["data"]]
             return results
-        return ModelIdentifier.identify_model(collection_name, request_body["data"])
+        return ModelIdentifier.identify_model(model_type, request_body["data"])

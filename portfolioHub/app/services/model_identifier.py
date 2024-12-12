1c340f88286ec00d2728e92678ee38010c17bdf2
JGVR <juangabrielvasquez11@gmail.com>
2024-12-12 12:53:19 -0500

    removed unused field_validator reference from the profile model class

--- portfolioHub/app/services/model_identifier.py
+++ portfolioHub/app/services/model_identifier.py
@@ -12,20 +12,7 @@ class ModelIdentifier:
             case "user":
                 return User(**data)
             case "profile":
-                return Profile(
-                    userId=data["userId"],
-                    firstName=data["firstName"],
-                    lastName=data["lastName"],
-                    dateOfBirth=data["dateOfBirth"],
-                    hobbies=data["hobby"],
-                    skills=data["skills"],
-                    shortBio = data["shortBio"],
-                    bio=data["bio"],
-                    countryOfBirth=data["countryOfBirth"],
-                    countryOfResidence=data["countryOfResidence"],
-                    linkedInUrl=data["linkedInUrl"],
-                    gitHubUrl=data["gitHubUrl"]
-                )
+                return Profile(**data)
             case "project":
                 return Project(**data)
             case "experience":

bbb01d31e479fea0bccee5a0f270b2d0af34863e
JGVR <juangabrielvasquez11@gmail.com>
2025-02-04 13:36:40 -0500

    added cors header middleware. Also allowed all origins to call the api for now

--- portfolioHub/portfolioHub/settings.py
+++ portfolioHub/portfolioHub/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# Allow all origins (not recommended for production)
+CORS_ALLOW_ALL_ORIGINS = True  
 
 ROOT_URLCONF = 'portfolioHub.urls'
 
